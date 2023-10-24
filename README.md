084b26405ac70ec37939ee48170c65f9b33e44ae
nozibuddowla <nozibuddowla@gmail.com>
2023-10-24 19:28:27 +0600

    add search funtionalities and delete all the books at once funtionlaties also

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # book-list
+
+A simple Book List using JavaScript, Bootstrap, and HTML, CSS
+
+Based on Brad Travery's Book List but with few changes.
+
+####Changes:
+- visual;
+- add some extra logic stuff {
+    v1:
+    - delete by title,
+    - responsive desktop, 
+    - required fields
+    v2:
+    - delete all books from list
+
+    v3:
+    filter by title, author, and publisher
+}
+
+- to do {
+    - filter by title, author and publisher (on list)
+    - delete all books from list
+    - owned, to buy tag
+    - ,etc
+}
--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+// Get the UI elements
+const form = document.querySelector('#book-form');
+const bookList = document.querySelector('#book-list');
+const deleteList = document.querySelector('#del-list-btn');
+const input = document.getElementById('myInput');
+
 // Book class: Represents a Book
 class Book {
     constructor(id, title, author, publisher) {
@@ -14,11 +20,12 @@ class UI {
 
         const row = document.createElement('tr');
 
+        row.classList.add('book-active');
         row.innerHTML = `
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.publisher}</td>
-        <td><a data-id="${book.id}" class='btn btn-danger btn-sm delete' href="#">X</a></td>
+        <td class="delete-btn"><a data-id="${book.id}" class='btn btn-danger btn-sm delete' href="#">X</a></td>
         `;
         list.appendChild(row);
     }
@@ -44,11 +51,12 @@ class UI {
 class Store {
     static getBooks() {
         let books;
-        if (localStorage.getItem('books') === null) {
-            books = [];
-        } else {
-            books = JSON.parse(localStorage.getItem('books'));
-        }
+        const getBooks = localStorage.getItem('books');
+
+        !getBooks ? books = [] : books = JSON.parse(localStorage.getItem('books'));
+
+        !books.length && (deleteList.disabled = true);
+
         return books;
     }
 
@@ -56,6 +64,7 @@ class Store {
         const books = Store.getBooks();
         book.id = generateUniqueId(); // Implement a function to generate unique IDs
         books.push(book);
+        deleteList.disabled = false;
         localStorage.setItem('books', JSON.stringify(books));
     }
 
@@ -66,19 +75,19 @@ class Store {
 
         if (index !== -1) {
             books.splice(index, 1);
+            books.length && (deleteList.disabled = true);
             localStorage.setItem('books', JSON.stringify(books));
         } 
     }
 }
 
-// Get the UI elements
-let form = document.querySelector('#book-form');
-let bookList = document.querySelector('#book-list');
 
 // Add Event Listener
 form.addEventListener('submit', newBook);
 bookList.addEventListener('click', removeBook);
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
+deleteList.addEventListener('click', deleteBooksList);
+
 
 // Define function
 function newBook(e) {
@@ -104,11 +113,45 @@ function newBook(e) {
 }
 
 function removeBook(e) {
-    if (e.target.classList.contains('delete')) {
+    if (e.target.classList.contains('delete') && e.target.hasAttribute('href')) {
         const id = e.target.getAttribute('data-id');
         UI.deleteBook(e.target);
         Store.removeBook(id);
     }
+}
+
+function deleteBooksList() {
+    bookList.parentNode.removeChild(bookList);
+    deleteList.disabled = true;
+    localStorage.removeItem('books');
+}
+
+function searchBook() {
+    const filter = input.value.toUpperCase();
+    const books = bookList.getElementsByClassName('book-active');
+
+    for (let index = 0; index < books.length; index++) {
+        const title = books[index].getElementsByTagName('td')[0];
+        const author = books[index].getElementsByTagName('td')[1];
+        const publisher = books[index].getElementsByTagName('td')[2];
+
+        if (title || author || publisher) {
+            const titleValue = title.textContent || title.innerText;
+            const authorValue = author.textContent || author.innerText;
+            const publisherValue = publisher.textContent || publisher.innerText;
+
+            if (filterBook(titleValue, filter) || filterBook(authorValue, filter) || filterBook(publisherValue, filter)) {
+                books[index].style.display = '';
+            } else {
+                books[index].style.display = 'none';
+            }
+        }
+        
+    }
+}
+
+const filterBook = (param, filter) => {
+    return param.toUpperCase().indexOf(filter) > - 1;
 }
 
 function generateUniqueId() {
